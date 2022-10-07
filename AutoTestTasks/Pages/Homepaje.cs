ccffefdb85bb06d14364c4f5cf78238e5f6cada5
Daria Martus <martusdarja@gmail.com>
2022-10-07 20:10:15 +0300

    Add project files.

---
+++ AutoTestTasks/Pages/Homepaje.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace AutoTestTasks.Pages
+{
+    public class Homepaje
+    {
+        private const string AutopracticeUrl = "http://automationpractice.com/index.php";
+        protected readonly IWebDriver driver;
+        public const int DefaultWaitInSeconds = 5;
+        public Homepaje(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+
+        public IWebElement searchField => driver.FindElement(By.Id("search_query_top"));
+        public IWebElement searchBtn => driver.FindElement(By.Name("submit_search"));
+
+        public void InsertWordInSearchfield(string text) => searchField.SendKeys(text);
+        public void ClickSearchBtn() => searchBtn.Click();
+    }
+}
