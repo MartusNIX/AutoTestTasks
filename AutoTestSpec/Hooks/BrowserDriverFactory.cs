c79a3f1a9714871c7ab3f068ea9923d9a50ed904
Martus_NIX <112709030+MartusNIX@users.noreply.github.com>
2022-10-09 12:41:45 +0300

    Multiple browser test using scenario tags (#2)
    * Created App.config. Trying to run tests on multiple browsers.
    
    * In progrerss
    
    * Trying to use Scenario's tag to launch browsers
    
    * Launched 6 scenarios on Chrome only
    
    * Fixed Edge, Firefox versions
    
    Co-authored-by: Daria Martus <martusdarja@gmail.com>

---
+++ AutoTestSpec/Hooks/BrowserDriverFactory.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace AutoTestSpec.Hooks
+{
+    public class BrowserDriverFactory
+    {
+        public IWebDriver GetWebDriver(string browserId)
+        {
+            switch (browserId)
+            {
+                case "Browser_Chrome": return GetChromeDriver();
+                case "Browser_Edge": return GetEdgeDriver();
+                case "Browser_Firefox": return GetFirefoxDriver();
+                default: throw new NotSupportedException("Browser is not supported by the BrowserDrivers");
+            }
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var chromeDriverService = ChromeDriverService.CreateDefaultService();
+            var chromeOptions = new ChromeOptions();
+            var chromeDriver = new ChromeDriver(chromeDriverService, chromeOptions);
+            return chromeDriver;
+        }
+
+        private IWebDriver GetEdgeDriver()
+        {
+            var edgeDriverService = EdgeDriverService.CreateDefaultService();
+            var edgeOptions = new EdgeOptions();
+            var edgeDriver = new EdgeDriver(edgeDriverService, edgeOptions);
+            return edgeDriver;
+
+        }
+
+        private IWebDriver GetFirefoxDriver()
+        {
+            var firefoxDriverService = FirefoxDriverService.CreateDefaultService();
+            var firefoxOptions = new FirefoxOptions();
+            var firefoxDriver = new FirefoxDriver(firefoxDriverService, firefoxOptions);
+            return firefoxDriver;
+        }
+    }
+}
